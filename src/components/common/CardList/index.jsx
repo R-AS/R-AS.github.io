6e7612a285de8000648e3738c53576edf461a24c
zhengjinyang <zhengjinyang@shein.com>
2021-03-04 21:58:53 +0800

    fix: fix anmiate

--- src/components/common/CardList/index.jsx
+++ src/components/common/CardList/index.jsx
@@ -3,10 +3,10 @@ import { Link } from 'gatsby'
 import { makeStyles } from '@material-ui/core/styles'
 import {
   Card, CardActionArea, CardMedia, CardContent,
-  CardActions, Typography, Grid,
+  CardActions, Typography, Grid, Slide,
 } from '@material-ui/core'
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     width: '100%',
@@ -21,6 +21,7 @@ const useStyles = makeStyles({
   card: {},
   media: {
     height: 120,
+    backgroundColor: theme.palette.warning.light,
   },
   title: {
     paddingTop: 8,
@@ -40,33 +41,37 @@ const useStyles = makeStyles({
     textOverflow: 'ellipsis',
     overflow: 'hidden',
   },
-})
+}))
 
-function CartItem({ title, date, thumbnail, slug }) {
+function CartItem({ title, date, thumbnail, slug, index }) {
   const classes = useStyles()
   return (
     <Grid item xs={12} sm={3}>
-      <Link className={classes.link} to={`/blogs${slug}`}>
-        <Card className={classes.card}>
-          <CardActionArea>
-            <CardMedia
-              className={classes.media}
-              image={`/thumbnail/${thumbnail}`}
-              title='Contemplative Reptile'
-            />
-            <CardContent className={classes.title}>
-              <Typography className={classes.cardTitle} variant='subtitle1' component='div'>
-                {title}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Typography className={classes.footer} align='right' gutterBottom variant='body2' component='span'>
-              {date}
-            </Typography>
-          </CardActions>
-        </Card>
-      </Link>
+      <Slide direction='down' in timeout={200 + (index * 30)} key={index}>
+        <div>
+          <Link className={classes.link} to={`/blogs${slug}`}>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={`/thumbnail/${thumbnail}`}
+                  title='Contemplative Reptile'
+                />
+                <CardContent className={classes.title}>
+                  <Typography className={classes.cardTitle} variant='subtitle1' component='div'>
+                    {title}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Typography className={classes.footer} align='right' gutterBottom variant='body2' component='span'>
+                  {date}
+                </Typography>
+              </CardActions>
+            </Card>
+          </Link>
+        </div>
+      </Slide>
     </Grid>
   )
 }
@@ -84,6 +89,7 @@ function CardList({ list }) {
           list.map((n, m) => (
             <CartItem
               key={m}
+              index={m}
               title={n.node.frontmatter.title}
               date={n.node.frontmatter.date}
               thumbnail={n.node.frontmatter.thumbnail}
